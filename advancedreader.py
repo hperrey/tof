21aabed073f230f5ba2bc24c839e62976b8b2d3b
Rasmus Kjaer Hoeier <nat14rkj@student.lu.se>
2018-06-12 11:29:01 +0200

    restructured code. calling reader and advanced reader to create first basic and then adv df from tofspectrum.py

--- advancedreader.py
+++ advancedreader.py
@@ -1,32 +1,42 @@
-import reader as rdr
-import cfd
-import pandas as pd
 import time
-import numpy as np
+import pandas as pd
+import cfd
 
-
-def pulse2dataframe(filename):
+def processframe(frame):
+    #Start the timer
     tstart=time.time()
-    frame=rdr.load_events(filename)
+    #Initiate the time reset counter.
     nTimeResets=0
+    #Initiate the columns of the advanced frame
+    #(those which are not directly inherited from the simple frame)
     tstamp=[0]*len(frame)
     refpoint=[0]*len(frame)
     noevent=[False]*len(frame)
     lzcross=[0]*len(frame)
     rzcross=[0]*len(frame)
+
+    #Loop through all the events
     for n in range(0,len(frame)):
         if n%1000==0:
             print('Event', n, '/', len(frame))
+        #For every event after the first check if the time
+        #has been reset. And keep count of the resets.
         if n>0:
             if frame.TimeStamp[n]<frame.TimeStamp[n-1]:
                 nTimeResets+=1
-        tstamp[n]=frame.TimeStamp[n]+nTimeResets*2147483647#+refpoint[n]
+        #Callibrate to nanoseconds needed!!!!!
+        tstamp[n]=frame.TimeStamp[n]+nTimeResets*2147483647
 
-        if np.count_nonzero(frame.Samples[n]) == 0:
-            noevent[n] = True
+        #Check if the event passes the threshold. This is
+        #to sort out empty and also tiny events.
+        if max(frame.Samples[n])<30:
+            noevent[n]=True
             continue
+        #For the remaining events (noevent=False) we
+        #find a reference point, using the cfd script.
         else:
             refpoint[n] = cfd.shifter(frame.Samples[n])
+            #And then we walk left and right to the zerocrossings.
             for u in range(refpoint[n]-1,-1,-1):
                 if frame.Samples[n][u]<1:
                     lzcross[n]=u
@@ -36,14 +46,16 @@ def pulse2dataframe(filename):
                     rzcross[n]=y
                     break
   
-
-    Frame=pd.DataFrame({'TimeStamp': tstamp,
+    #Create the expanded dataframe.
+    Frame=pd.DataFrame({'Timestamp': tstamp,
                         'Samples' : frame.Samples,
                         'Baseline' : frame.Baseline,
-                        'refpoint':refpoint,
+                        'Refpoint':refpoint,
                         'Left':lzcross,
                         'Right': rzcross,
-                        'noevent': noevent})
+                        'Noevent': noevent})
+
+    #stop timing and print the runtime
     tstop=time.time()
     print('processing time = ',tstop-tstart)
     return Frame
--- tofSpectrum.py
+++ tofSpectrum.py
@@ -1,11 +1,34 @@
-Tlist=[]
-i=0
-for tn in ne213.TimeStamp:
-    for tg in yap.TimeStamp:
-        if -1500<tg-tn<1500:
-            Tlist.append(tg-tn)
-    if i%1000==0:
-        print(i,'/',len(ne213))
-    i+=1
+#import reader as rdr
+#import advancedreader as adv
+import matplotlib.pyplot as plt
+import pandas as pd
 
-plt.hist(Tlist,900,range=(-450,450));plt.show()
+#simpleNeutron=rdr.load_events('data/ne213.txt')
+#simpleGamma=rdr.load_events('data/yap.txt')
+#neutron=adv.processframe(simpleNeutron)
+#gamma=adv.processframe(simpleGamma)
+neutron=pd.read_hdf('data/ne213jun11th2018.h5')
+gamma=pd.read_hdf('data/yapjun11th2018.h5')
+print('Frames have been loaded')
+
+
+
+
+
+Tlist=[]
+#nelist=[]
+#ylist=[]
+#this needs to be speeded up!
+for ne in range(0, len(neutron)):
+    if ne%10==0:
+        print(ne,'/',len(neutron))
+    if neutron.Noevent[ne]==False:
+        for y in range(0, len(gamma)):
+            if gamma.Noevent[ne]==False:
+                if -12000 < (neutron.Timestamp[ne]+neutron.Refpoint[ne])-(gamma.Timestamp[y]+gamma.Refpoint[y]) <12000:
+                    Tlist.append((neutron.Timestamp[ne]+neutron.Refpoint[ne])-(gamma.Timestamp[y]+gamma.Refpoint[y]))
+                    #nelist.append(ne)
+                    #ylist.append(y)
+plt.hist(Tlist,200)
+pl.show()
+
