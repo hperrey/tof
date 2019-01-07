28a86a2bc61373134f6cec224285c4e30ec674b8
Rasmus Kjaer Hoeier <nat14rkj@student.lu.se>
2019-01-07 15:30:52 +0100

    load_data() works, but 10+gB files are still slow to process.

--- tof.py
+++ tof.py
@@ -8,18 +8,25 @@ import time
 from math import sqrt
 from math import atan
 
-def load_data(filename, threshold, frac=0.3, nlines=0, startline=0, nTimesReset=0, no_skip=False, chunksize=5000, outpath='data/chunk'):
+def load_data(filename, threshold, frac=0.3, nlines=0, startline=0, nTimesReset=0, no_skip=False, chunksize=2**18, outpath='data/chunk'):
+    t0 = time.time() 
+    print("Scanning the file to get number of chunks:")
+    nChunks = int(round(0.5 + sum(1 for row in open(filename, 'r'))/chunksize))
+    t1 = time.time()
+    print("Scan time: ", t1-t0, ' seconds')
+    print("Will generate ", nChunks)
     Chunks = pd.read_csv(filename, header=None, usecols=[5,7], names=['timestamp', 'samples'], chunksize=chunksize)
     count=0
+    tdummy1=t1
     for df in Chunks:
-        print("Chunk number", count)
+        tdummy2=tdummy1
+        print("Chunk number", count + 1, "/", nChunks)
         df['samples'] = df.samples.str.split().apply(lambda x: np.array(x, dtype=np.int16))
 
         samples = np.array([None]*df.shape[0])
         timestamp = np.array([0]*df.shape[0], dtype=np.int64)
         amplitude = np.array([0]*df.shape[0], dtype=np.int16)
         peak_index = np.array([0]*df.shape[0], dtype=np.int16)
-        print('len peak_index ',len(peak_index))
         valid_event = np.array([True]*df.shape[0], dtype=np.int16)
         ref_point_rise = np.array([0]*df.shape[0], dtype=np.int32)
         ref_point_fall = np.array([0]*df.shape[0], dtype=np.int32)
@@ -58,6 +65,8 @@ def load_data(filename, threshold, frac=0.3, nlines=0, startline=0, nTimesReset=
         df.to_hdf(outpath+'.h5', key='key%s'%count)
         df = df.drop('samples', axis = 1)
         df.to_hdf(outpath+'cooked.h5', key='key%s'%count)
+        tdummy1=time.time()
+        print('chunk ', count, ' processed in ', tdummy1-tdummy2, ' seconds'  )
         count += 1
 
 
